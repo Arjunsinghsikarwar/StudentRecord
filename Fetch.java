5f70988bf715c370e3988a4db4c6dda54975bd3e
Arjunsinghsikarwar <140879142+Arjunsinghsikarwar@users.noreply.github.com>
2025-05-15 11:42:18 +0530

    Fetch.java

--- Fetch.java
+++ Fetch.java
@@ -2,27 +2,21 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Fetch
-{
+public class Fetch {
     public static void main(String[] args) {
-        System.out.println("In this we fetch the data"); //By using hibernate.
-        Configuration configuration = new Configuration().configure().addAnnotatedClass(Side.class);  // here we put the class name in which entity class is.
+        System.out.println("In this example, we fetch data using Hibernate.");
+
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Side.class);  
+        // We add the annotated entity class here.
+
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
-        // Here we don't need to use the transaction , since it only uses to make the changes and update in the table.
 
-        // Remeber this , we have to method in session to fetch the data from the data base
-        // 1. get() -->  in this case if we get() the the query got executed  even if we used or not.
-        //2. load(). --> in this case if use the lod() the query only executed when only it got used.
+        // Note: We don't need a transaction here since we are not making any changes to the table,
+        // we are only reading (fetching) data.
 
-       Side student =  session.get(Side.class,12);
+        // Remember this: Hibernate provides two methods to fetch data from the database:
+        // 1. get()  --> Executes the query immediately, whether you use the data or not.
+        // 2. load() --> Returns a proxy and delays the query execution until you actually use the object.
 
-        System.out.println(student);
-
-        session.close();  // just for the good prartice.
-
-        // Side student = session.load(Side,class,102);
-        // this query will only exectue when it is used to get somethingh.
-
-    }
-}
+        Side student = session.get(Side.class, 12); // Fetches the student with
