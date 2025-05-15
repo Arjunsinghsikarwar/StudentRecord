7e6eb369f615493884a3a1782e695ddc47f65d74
Arjunsinghsikarwar <140879142+Arjunsinghsikarwar@users.noreply.github.com>
2025-05-15 11:41:40 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -3,31 +3,33 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-class Main
-{
-    public static void main(String[] args)
-    {
+class Main {
+    public static void main(String[] args) {
         Side student = new Side();
-      //  System.out.println(side);  after seeign the result of this , try to reliaze why we Overiide the toString() method.
+        // System.out.println(student);  
+        // After seeing the result of this, try to understand why we override the toString() method.
 
         student.setName("Munish");
-        student.setEducation("B.tech");
+        student.setEducation("B.Tech");
         student.setRool_Number(69);
 
-        Configuration configuration = new Configuration().configure().addAnnotatedClass(Side.class); // the class jiska humhe data save krna hain.
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Side.class); 
+        // Add the annotated class whose data we want to save.
+
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();  // To make the permentat changes we need this .
-        session.save(student);
-        transaction.commit(); // here changes have succesfully
+        Transaction transaction = session.beginTransaction();  
+        // To make permanent changes in the database, we need a transaction.
 
-        // hum session.save(student); ko try and catch likhte hain , b/c transaction have the method rollback ke agr Exception aayge toh chize roolback ho jaaye.
+        session.save(student);
+        transaction.commit(); 
+        // Changes are successfully committed here.
+
+        // We usually wrap session.save(student) in a try-catch block,
+        // because the transaction object has a rollback() method which ensures
+        // that if any exception occurs, the changes will be rolled back.
 
         System.out.println(student);
 
-
-//         <property name="hibernate.hbm2ddl.auto">update</property>
-        // this proeprty is used to make a table by itself , name we make the updated because it will the update the and table , and if the tbale do not had created previously then it will make onece.
-
-    }
-}
+        // <property name="hibernate.hbm2ddl.auto">update</property>
+        // This property is used to automatically create a table
